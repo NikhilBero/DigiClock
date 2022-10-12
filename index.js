1cf2158b87c51b21cce733f98a9b0c19ef84b17a
UnTiTled <104919147+NikhilBero@users.noreply.github.com>
2022-10-12 04:30:03 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ function updatetime(){
 let h = new Date().getHours()
 let m = new Date().getMinutes()
 let s = new Date().getSeconds()
-let ampm = "PM" 
+let ampm = "AM" 
 
 if( h > 12){
 
     h = h-12
-    ampm = "AM"
+    ampm = "PM"
 }
     h = h < 10 ? "0" + h : h;    
     m = m < 10 ? "0" + m : m;   
