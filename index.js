4b4b7044b0cefd5fe1af6e0efcd740cb83faf8e6
UnTiTled <104919147+NikhilBero@users.noreply.github.com>
2022-10-12 04:20:21 +0100

    Update index.js

--- index.js
+++ index.js
@@ -12,8 +12,11 @@ let ampm = "PM"
 if( h > 12){
 
     h = h-12
-    ampm = "PM"
+    ampm = "AM"
 }
+    h = h < 10 ? "0" + h : h;    
+    m = m < 10 ? "0" + m : m;   
+    s = s < 10 ? "0" + s : s;   
 
 hrsEl.innerText = h ;
 minEl.innerText = m ;
